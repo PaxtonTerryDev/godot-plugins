890d7b2317d70036c3bd8b97455d4bf95e5a9aac
Paxton Terry <paxtonterrydev@gmail.com>
2026-02-18 10:20:08 -0700

    feat: updates Syslog with several qol features

--- addons/application_environment/src/application.gd
+++ addons/application_environment/src/application.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 	logger.info("ready - current environment: %s" % env())
 	logger.info("current data directory: %s " % get_data_dir_path())
 	_ensure_directories()
+
 func is_dev() -> bool:
 	return OS.has_feature("editor")
 
@@ -15,10 +16,11 @@ func env() -> String:
 const DEV_DATA_DIR: String = "res://tmp/data"
 const PROD_DATA_DIR: String = "user://"
 
+func get_data_dir_path() -> String:
+	return DEV_DATA_DIR if is_dev() else PROD_DATA_DIR
+
 func _ensure_directories() -> void:
 	logger.info("ensuring existence of data directories")
 	DirAccess.make_dir_recursive_absolute(get_data_dir_path())
-	logger.info("directories verified")
 
-func get_data_dir_path() -> String:
-	return DEV_DATA_DIR if is_dev() else PROD_DATA_DIR
+
--- addons/logger/syslog.gd
+++ addons/logger/syslog.gd
@@ -1,10 +1,28 @@
 class_name Syslog extends RefCounted
 
+const GLOBAL_VERBOSITY: Verbosity = Verbosity.NORMAL
+
 const _MAX_LEVEL_LEN: int = 8  # length of "[ERROR] "
 static var _max_scope_display_len: int = 0
 
+const ENABLED_LEVELS_VERBOSITY: Dictionary[String, Verbosity] = {
+	"DEBUG": Verbosity.VERBOSE,
+	"INFO": Verbosity.NORMAL,
+	"WARN": Verbosity.NORMAL,
+	"ERROR": Verbosity.QUIET
+	}
+
+func is_log_enabled(level: String) -> bool:
+	return _verbosity >= ENABLED_LEVELS_VERBOSITY[level]
+
+enum Verbosity {
+	QUIET,
+	NORMAL,
+	VERBOSE
+}
+var _verbosity: Verbosity = Verbosity.NORMAL
 var scopes: Dictionary[String, bool] = {}
-var normalize: bool = true
+var _normalize: bool = false
 
 func format_scope() -> String:
 	var active_scopes: Array[String] = []
@@ -30,7 +48,7 @@ func enable_scope(target_scope: String) -> void:
 func temp_scope(_temp_scope: String) -> Syslog:
 	var t = Syslog.new()
 	t.scopes = scopes.duplicate()
-	t.normalize = normalize
+	t._normalize = _normalize
 	t.scopes.set(_temp_scope, true)  # bypass add_scope — temp scopes don't affect global max
 	return t
 
@@ -45,7 +63,7 @@ func _update_max_scope_len() -> void:
 func format_log(_level: String = "", _message: String = "") -> String:
 	var _scope = format_scope()
 	var formatted_message = ""
-	if normalize:
+	if _normalize:
 		var level_part = "[%s] " % _level if _level.length() > 0 else ""
 		formatted_message += level_part.rpad(_MAX_LEVEL_LEN)
 		var scope_part = "(%s)  " % _scope if _scope.length() > 0 else ""
@@ -62,28 +80,34 @@ static func print_spacer() -> void:
 
 ## Print a debug log -> this does not include a level, but does include a scopes.  If you do not want a scopes, you can just call `print` directly
 func debug(message: String, spacer: bool = false) -> void:
+	if !is_log_enabled("DEBUG"): return
 	print(format_log("", message))
 	if spacer: Syslog.print_spacer()
 
 ## Prints an info log
 func info(message: String, spacer: bool = false) -> void:
+	if !is_log_enabled("INFO"): return
 	print(format_log("INFO", message))
 	if spacer: Syslog.print_spacer()
 
 ## Prints a warning log
 func warn(message: String, push_warning: bool = true, spacer: bool = false) -> void:
+	if !is_log_enabled("WARN"): return
 	print(format_log("WARN", message))
 	if push_warning: push_warning(format_log("", message))
 	if spacer: Syslog.print_spacer()
 
 ## Prints an error log
 func error(message: String, push_error: bool = true, spacer: bool = false) -> void:
+	if !is_log_enabled("ERROR"): return
 	print(format_log("ERROR", message))
 	if push_error: push_error(format_log("", message))
 	if spacer: Syslog.print_spacer()
 
-func _init(initial_scopes: PackedStringArray = []) -> void:
+func _init(initial_scopes: PackedStringArray = [], normalize: bool = false, verbosity: Verbosity = GLOBAL_VERBOSITY,) -> void:
 	for s in initial_scopes:
 		scopes.set(s, true)
 	if not scopes.is_empty():
 		_update_max_scope_len()
+	_normalize = normalize
+	_verbosity = verbosity
